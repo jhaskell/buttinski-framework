04d5e43c042a4650588fd4c9bb108343a1442a7a
James Haskell <jhaskell@jhaskell.net>
2013-09-11 14:44:17 -0700

    Fixes parent class for User entity

--- src/GBS/EntityBundle/Document/User.php
+++ src/GBS/EntityBundle/Document/User.php
@@ -3,7 +3,7 @@
 namespace GBS\EntityBundle\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
-use FOS\UserBundle\Model\User as BaseUser;
+use FOS\UserBundle\Document\User as BaseUser;
 
 class User extends BaseUser
 {
