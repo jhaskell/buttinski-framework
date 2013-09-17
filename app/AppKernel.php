b85ea7c19f5c5cce87ba78c6f41b2e75b05842f4
James Haskell <jhaskell@jhaskell.net>
2013-09-16 20:36:20 -0700

    Adding doctrine fixtures bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new GBS\EntityBundle\GBSEntityBundle(),
             new GBS\LocationBundle\GBSLocationBundle(),
             new GBS\EveAPIBundle\GBSEveAPIBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
